e86196e9d026504982062788995e3fa59614a938
RaulCuart <raulcuartortega@a.cifpperedesongall.org>
2025-04-23 22:38:01 +0200

    añadidas animaciones

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,101 +8,102 @@ public class Player : MonoBehaviour
     public Rigidbody2D rb;
     private Vector2 movimiento;
     public Collider2D playerCollider;
+    public Animator animator;
+    private bool hasSword = false;
+    public bool isMoving = false;
 
-    public Sprite[] spritesAbajo;
-    public Sprite[] spritesArriba;
-    public Sprite[] spritesIzquierda;
-    public Sprite[] spritesDerecha;
-    private Sprite[] animacionActual;
+    public GameObject attackObjectprefab;
+    public Vector2 ultimaDireccion = Vector2.down;
 
-    public float animationTime = 0.2f;
-    private SpriteRenderer spriteRenderer;
-    private int animationFrame;
-    private bool estaCaminando = false;
 
-    private Sprite ultimoSprite;
-
-    void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animacionActual = spritesAbajo; 
-        ultimoSprite = animacionActual[0]; 
-    }
 
     void Update()
     {
-
+        isMoving = false;
+        animator.SetBool("isMoving", isMoving);
         movimiento.x = 0;
         movimiento.y = 0;
-        estaCaminando = false;
+        animator.SetFloat("movimiento.x", movimiento.x);
+        animator.SetFloat("movimiento.y", movimiento.y);
+
 
         if (Input.GetKey(KeyCode.A))
         {
             movimiento.x = -1;
-            estaCaminando = true;
-            animacionActual = spritesIzquierda;
+            animator.SetFloat("movimiento.x", movimiento.x);
+            isMoving = true;
+            animator.SetBool("isMoving", isMoving);
+            ultimaDireccion = Vector2.left;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             movimiento.x = 1;
-            estaCaminando = true;
-            animacionActual = spritesDerecha;
+            animator.SetFloat("movimiento.x", movimiento.x);
+            isMoving = true;
+            animator.SetBool("isMoving", isMoving);
+            ultimaDireccion = Vector2.right;
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             movimiento.y = 1;
-            estaCaminando = true;
-            animacionActual = spritesArriba;
+            animator.SetFloat("movimiento.y", movimiento.y);
+            isMoving = true;
+            animator.SetBool("isMoving", isMoving);
+            ultimaDireccion = Vector2.up;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
             movimiento.y = -1;
-            estaCaminando = true;
-            animacionActual = spritesAbajo;
+            animator.SetFloat("movimiento.y", movimiento.y);
+            isMoving = true;
+            animator.SetBool("isMoving", isMoving);
+            ultimaDireccion = Vector2.down;
+
         }
 
-        //Para que no camine mas rapido de forma diagonal
-        movimiento = movimiento.normalized;
-
-        if (estaCaminando)
+        if (Input.GetKeyDown(KeyCode.Space) && hasSword)
         {
-            AnimateSprite();
-        }
-        else
-        {
-            animationFrame = 0;
-            spriteRenderer.sprite = ultimoSprite;
+            Attack();
         }
     }
 
     void FixedUpdate()
     {
-        
+
         rb.MovePosition(rb.position + movimiento * velocidad * Time.fixedDeltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-       
+
         if (collision.collider.CompareTag("Enemy"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Sword"))
+        {
+            hasSword = true;
+            Destroy(other.gameObject);
+        }
+        if (other.CompareTag("Enemy"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
-    private void AnimateSprite()
+    private void Attack()
     {
-        animationFrame++;
+        Vector2 posicionAtaque = (Vector2)transform.position + ultimaDireccion;
+        Instantiate(attackObjectprefab, posicionAtaque, Quaternion.identity);
 
-        if (animationFrame >= animacionActual.Length)
-        {
-            animationFrame = 0;
-        }
-
-        spriteRenderer.sprite = animacionActual[animationFrame];
-        ultimoSprite = spriteRenderer.sprite; 
     }
+
 }
