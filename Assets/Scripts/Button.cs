4fabe9115b316e585f17d7cd824010dacc634119
RaulCuart <raulcuartortega@a.cifpperedesongall.org>
2025-05-01 21:32:57 +0200

    Añadidos sonidos ambientales, sonidos de pasos, botones y botones trampa, añadido botón para linterna y shifting (sigilo)

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject buttonObject;
+    public GameObject pitTrap;
+    public BoxCollider2D activationArea;
+    public GameObject player;
+    public GameObject playerFlashLight;
+    public Boolean isTrap;
+    private Boolean canBeToggled = false;
+    private Boolean activated = false;
+    public AudioSource fallingEffect;
+    private bool playerIsAlive;
+    public AudioSource ambientalMusic;
+   
+    void Start()
+    {
+         playerIsAlive = player.GetComponent<Player>().isAlive;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && canBeToggled && playerIsAlive)
+        {
+            if (!activated)
+            {
+                buttonObject.GetComponent<SpriteRenderer>().enabled = true;
+                activated = true;
+                if (isTrap)
+                {
+                    fall();
+                }
+            } else
+            {
+                buttonObject.GetComponent<SpriteRenderer>().enabled = false;
+                activated = false;
+            }
+
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            canBeToggled=true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")) {
+            canBeToggled=false;
+        }
+    }
+
+    private void fall()
+    {
+        pitTrap.SetActive(true);
+        fallingEffect.Play();
+        player.GetComponent<SpriteRenderer>().enabled = false;
+        playerIsAlive = false;
+        player.GetComponent<Player>().isAlive = playerIsAlive;
+        playerFlashLight.SetActive(false);
+        ambientalMusic.Stop();
+    }
+
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,45 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public bool isShifting = false;
     public float velocidad = 5f;
     public Rigidbody2D rb;
     private Vector2 movimiento;
     public Collider2D playerCollider;
     public Animator animator;
-    public Transform flashlightRotation;
+    public GameObject flashlight;
     private bool hasSword = false;
+    private float normalSpeed;
     public bool isMoving = false;
-   
+    public bool isAlive = true;
+    public AudioSource gameOver;
+    private bool musicPlayed = false;
+    public AudioSource steps;
+
 
     public GameObject attackObjectprefab;
     public Vector2 ultimaDireccion = Vector2.down;
 
-
+    private void Start()
+    {
+        normalSpeed = velocidad;
+    }
 
     void Update()
-    {
+    {   
+        if (!isAlive && !musicPlayed)
+        {
+            StartCoroutine(PlayGameOverSound(3f));
+            musicPlayed=true;
+        }
+        velocidad = normalSpeed;
         isMoving = false;
+        isShifting = false;
         animator.SetBool("isMoving", isMoving);
         movimiento.x = 0;
         movimiento.y = 0;
@@ -30,56 +47,82 @@ public class Player : MonoBehaviour
         animator.SetFloat("movimiento.y", movimiento.y);
 
 
-        if (Input.GetKey(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.LeftShift))     
+        {
+            velocidad = normalSpeed / 2;
+            animator.speed = 0.3f;
+            isShifting = true;
+        } else
+        {
+            animator.speed = 1f;
+        }
+
+        if (Input.GetKey(KeyCode.A) && isAlive)
         {
             movimiento.x = -1;
             animator.SetFloat("movimiento.x", movimiento.x);
             isMoving = true;
             animator.SetBool("isMoving", isMoving);
             ultimaDireccion = Vector2.left;
-            flashlightRotation.transform.rotation = Quaternion.Euler(0, 0, 90);
-
+            flashlight.transform.rotation = Quaternion.Euler(0, 0, 90);
+            playStepSound();
         }
 
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) && isAlive)
         {
             movimiento.x = 1;
             animator.SetFloat("movimiento.x", movimiento.x);
             isMoving = true;
             animator.SetBool("isMoving", isMoving);
             ultimaDireccion = Vector2.right;
-            flashlightRotation.transform.rotation = Quaternion.Euler(0, 0, 270);
+            flashlight.transform.rotation = Quaternion.Euler(0, 0, 270);
+            playStepSound();
+
         }
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W) && isAlive)
         {
             movimiento.y = 1;
             animator.SetFloat("movimiento.y", movimiento.y);
             isMoving = true;
             animator.SetBool("isMoving", isMoving);
             ultimaDireccion = Vector2.up;
-            flashlightRotation.transform.rotation = Quaternion.Euler(0, 0, 0);
+            flashlight.transform.rotation = Quaternion.Euler(0, 0, 0);
+            playStepSound();
+
         }
 
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) && isAlive)
         {
             movimiento.y = -1;
             animator.SetFloat("movimiento.y", movimiento.y);
             isMoving = true;
             animator.SetBool("isMoving", isMoving);
             ultimaDireccion = Vector2.down;
+            flashlight.transform.rotation = Quaternion.Euler(0, 0, 180);
+            playStepSound();
 
-            flashlightRotation.transform.rotation = Quaternion.Euler(0, 0, 180);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && hasSword)
         {
             Attack();
         }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            toggleFlashlight();
+        }
+
+        stopStepSound();
     }
 
     void FixedUpdate()
     {
+        if (movimiento.x != 0 && movimiento.y != 0)
+        {
+            movimiento.Normalize();
+        }
 
         rb.MovePosition(rb.position + movimiento * velocidad * Time.fixedDeltaTime);
     }
@@ -114,4 +157,38 @@ public class Player : MonoBehaviour
 
     }
 
+    private void toggleFlashlight()
+    {
+        Light2D light = flashlight.GetComponent<Light2D>();
+        if (light.intensity > 0)
+        {
+            light.intensity = 0;
+        } 
+        else
+        {
+            light.intensity = 1;
+        }
+    }
+
+    IEnumerator PlayGameOverSound(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        gameOver.Play();
+    }
+
+    private void playStepSound()
+    {
+        if (!steps.isPlaying && !isShifting && isMoving)
+        {
+            steps.Play();
+        }
+    }
+
+    private void stopStepSound()
+    {
+        if (!isMoving && steps.isPlaying || isShifting && steps.isPlaying)
+        {
+            steps.Stop();
+        }
+    }
 }
